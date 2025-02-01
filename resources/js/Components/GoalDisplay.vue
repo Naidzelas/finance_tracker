ff4bed17c403f7f768c878d007ccbd8803a5414a
naidzelas <naidzelas.benetis@gmail.com>
2025-02-01 14:47:59 +0200

    Some bug fixes

--- resources/js/Components/GoalDisplay.vue
+++ resources/js/Components/GoalDisplay.vue
@@ -1,33 +1,33 @@
 <template>
-    <div v-for="goal in goal_data" class="mb-2 group relative">
-        <!-- <EditOrDelete :route="{edit:'goal.edit', delete:'goal.destroy'}" :id="goal.id"></EditOrDelete> -->
-        <div
-            class="flex w-0 gap-2 p-2 absolute top-[25%] right-10 invisible group-hover:visible cursor-pointer group-hover:transition-all group-hover:ease-in-out hover:scale-110 group-hover:w-32"
+    <div v-for="goal in goal_data" class="group relative mb-2">
+        <EditOrDelete :action="{edit:'goal.edit', delete:'goal.destroy'}" :id="goal.id"></EditOrDelete>
+        <!-- <div
+            class="group-hover:visible group-hover:ease-in-out group-hover:w-32 top-[25%] right-10 absolute flex gap-2 p-2 w-0 group-hover:transition-all cursor-pointer invisible hover:scale-110"
         >
             <Link
                 :href="route('goal.edit', { id: goal.id })"
                 as="button"
-                class="bg-[#297A9D] hover:bg-[#153d4f] flex-1 text-white rounded h-8 grid place-items-center"
+                class="flex-1 place-items-center grid bg-[#297A9D] hover:bg-[#153d4f] rounded h-8 text-white"
             >
                 <Icon icon="tabler:edit" class="size-6"></Icon>
             </Link>
             <button
                 @click="deleteItem(goal.id)"
-                class="bg-[#5A0000] hover:bg-[#942929] flex-1 text-white rounded h-8 grid place-items-center"
+                class="flex-1 place-items-center grid bg-[#5A0000] hover:bg-[#942929] rounded h-8 text-white"
             >
                 <Icon icon="fa6-solid:trash" class="size-4"></Icon>
             </button>
-        </div>
-        <div class="rounded-full mb-0.5 bg-[white]">
+        </div> -->
+        <div class="bg-[white] mb-0.5 rounded-full">
             <div class="bg-black w-[25%] h-1" :class="percent(goal)"></div>
         </div>
         <div
-            class="flex p-4 place-items-center rounded border-6 bg-[white] text-center"
+            class="flex place-items-center border-6 bg-[white] p-4 rounded text-center"
         >
             <div class="flex flex-auto justify-center">
                 <Icon :icon="goal.icon.iconify_name" class="size-10"></Icon>
             </div>
-            <div class="w-96 text-3xl font-semibold">{{ goal.name }}</div>
+            <div class="w-96 font-semibold text-3xl">{{ goal.name }}</div>
             <div class="flex-auto text-3xl group-hover:invisible">{{ goal.end_goal }}</div>
         </div>
     </div>

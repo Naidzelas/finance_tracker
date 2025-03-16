70e1c80564d3881fde2810bf92916ddd328b7f51
naidzelas <naidzelas.benetis@gmail.com>
2025-03-16 19:42:22 +0200

    Small changes

--- resources/js/Components/Nav.vue
+++ resources/js/Components/Nav.vue
@@ -5,10 +5,7 @@
             <button @click="navToggle(true)">
                 <Icon icon="ph:cube-fill" class="mt-1 size-10"></Icon>
             </button>
-            <div
-                v-if="menu_visible"
-                @pointerleave="navToggle(false)"
-            >
+            <div v-if="menu_visible" @pointerleave="navToggle(false)">
                 <div
                     class="top-0 right-0.5 absolute bg-white rounded-r-md w-20 size-12"
                 >
@@ -32,7 +29,11 @@
                     <Icon icon="basil:add-solid" class="mt-4 size-6"></Icon>
                 </button>
             </div>
-            <Link v-if="page.url.split('/').length < 3 && page.url.split('/')[1].length"
+            <Link
+                v-if="
+                    page.url.split('/').length < 3 &&
+                    page.url.split('/')[1].length
+                "
                 :href="page.url + '/create'"
                 ><Icon icon="basil:add-solid" class="mt-4 size-6"></Icon
             ></Link>
@@ -51,8 +52,8 @@
     </div>
 </template>
 <script setup>
-import { reactive, provide, ref, onMounted } from "vue";
-import { router, Link, usePage } from "@inertiajs/vue3";
+import { provide, ref, onMounted } from "vue";
+import { Link, usePage } from "@inertiajs/vue3";
 import NavItems from "../Components/NavItems.vue";
 import ImportFileModal from "./ImportFileModal.vue";
 import "vue-toastification/dist/index.css";

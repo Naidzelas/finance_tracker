528451c1625f34200d28d9522d8c8ac476c9a164
naidzelas <naidzelas.benetis@gmail.com>
2025-03-16 12:55:34 +0200

    i18n bug fix. Problem was the i18n was created in app.js so that when a hard reload would happen it would not trigger the createI18n and would go straight to createInertiaApp resolve.

--- resources/js/Layout/Layout.vue
+++ resources/js/Layout/Layout.vue
@@ -24,44 +24,7 @@
                             >{{ $t("general.profile") }}</Link
                         >
                     </div>
-                    <div class="top-0 -left-16 absolute flex gap-2">
-                        <input
-                            @change="changeLanguage('lt')"
-                            type="radio"
-                            id="lang-lt"
-                            name="language"
-                            ref="checked"
-                            class="sr-only peer/lt"
-                            :checked="i18n.global.locale.value === 'lt'"
-                        />
-                        <label
-                            for="lang-lt"
-                            class="opacity-20 hover:opacity-100 peer-checked/lt:opacity-100 p-0.5 rounded-full hover:scale-110 peer-checked/lt:scale-110 transition duration-200 ease-in-out cursor-pointer"
-                        >
-                            <Icon
-                                icon="circle-flags:lang-lt"
-                                class="size-6"
-                            ></Icon>
-                        </label>
-                        <input
-                            @change="changeLanguage('en')"
-                            type="radio"
-                            id="lang-en"
-                            name="language"
-                            ref="checked"
-                            class="sr-only peer/en"
-                            :checked="i18n.global.locale.value === 'en'"
-                        />
-                        <label
-                            for="lang-en"
-                            class="peer-checked/en:disabled opacity-20 hover:opacity-100 peer-checked/en:opacity-100 p-0.5 rounded-full hover:scale-110 peer-checked/en:scale-110 transition duration-200 ease-in-out cursor-pointer"
-                        >
-                            <Icon
-                                icon="circle-flags:lang-en"
-                                class="size-6"
-                            ></Icon>
-                        </label>
-                    </div>
+                    <LanguageSelect></LanguageSelect>
                 </div>
             </div>
             <Nav></Nav>
@@ -72,15 +35,9 @@
 <script setup>
 import { Link, usePage } from "@inertiajs/vue3";
 import { computed } from "vue";
-import { Icon } from "@iconify/vue/dist/iconify.js";
 import Nav from "../Components/Nav.vue";
-import { i18n } from "../app";
+import LanguageSelect from "../Components/LanguageSelect.vue";
 
 const page = usePage();
 const user = computed(() => page.props.auth.user);
-
-const changeLanguage = (locale) => {
-    i18n.global.locale.value = locale;
-    localStorage.setItem("locale", locale);
-};
 </script>

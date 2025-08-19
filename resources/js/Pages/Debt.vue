6cd9abd3ede28f365d8632fbbe265c35550443bd
naidzelas <naidzelas.benetis@gmail.com>
2025-08-19 20:35:44 +0300

    Enhance Debt and Investment pages with AppHeader component; update layout and add delete functionality for debts and investments

--- resources/js/Pages/Debt.vue
+++ resources/js/Pages/Debt.vue
@@ -1,9 +1,11 @@
 <template>
     <section class="flex flex-col 2xl:mr-40 2xl:ml-40">
-        <div class="mb-10 text-5xl 2xl:text-left text-center">
-            {{ $t("debts.title").toLocaleLowerCase() }}
-        </div>
-        <Breadcrumbs :breadcrumbs="breadcrumbs"></Breadcrumbs>
+        <AppHeader
+            :breadcrumbs="items"
+            title="Debts"
+            subtitle="Track your debts"
+            class="mb-10"
+        ></AppHeader>
         <div
             v-if="Object.keys(debts).length"
             v-for="debt in debts"
@@ -12,11 +14,11 @@
             <LvProgressBar
                 :value="getPercent(debt.loan_final_amount, debt.paid)"
                 :color="'#008ba0'"
+                :showValue="false"
                 class="bg-white mb-0.5 rounded-md h-2"
             ></LvProgressBar>
-
             <div
-                class="md:flex gap-4 grid grid-cols-3 bg-[#F4F4F4] -mb-4 p-3 rounded-md md:text-md text-sm"
+                class="md:flex gap-4 grid grid-cols-3 bg-surface-0 shadow-lg -mb-4 p-3 rounded-md md:text-md text-sm"
             >
                 <div class="flex flex-1 justify-center col-span-3 md:pl-10">
                     <div>
@@ -48,9 +50,7 @@
                     <div class="font-bold">{{ $t("debts.interest_rate") }}</div>
                     <div>{{ debt.interest_rate }}</div>
                 </div>
-                <div
-                    class="group-hover:invisible flex flex-col flex-1 text-center"
-                >
+                <div class="flex flex-col flex-1 text-center">
                     <div class="font-bold">{{ $t("debts.loan_payed") }}</div>
                     <div>
                         {{
@@ -60,19 +60,19 @@
                         }}
                     </div>
                 </div>
+                <Button
+                    rounded
+                    icon="pi pi-trash"
+                    severity="danger"
+                    class="md:mr-4"
+                    :outlined="true"
+                    @click="deleteDebt(debt.id)"
+                ></Button>
             </div>
             <DetailsDisplay
                 :detailsTab="detailsTab"
                 :id="debt.id"
             ></DetailsDisplay>
-            <EditOrDelete
-                :action="{
-                    edit: 'debt.edit',
-                    delete: 'debt.destroy',
-                    redirect: '/debt',
-                }"
-                :id="debt.id"
-            ></EditOrDelete>
         </div>
         <NoData v-else></NoData>
     </section>
@@ -80,11 +80,12 @@
 
 <script setup>
 import DetailsDisplay from "../Components/DetailsDisplay.vue";
-import EditOrDelete from "../Components/EditOrDelete.vue";
 import NoData from "../Components/NoData.vue";
 import LvProgressBar from "lightvue/progress-bar";
-import Breadcrumbs from "../Components/Breadcrumbs.vue";
-import { provide } from "vue";
+import { provide, ref } from "vue";
+import { Link, router } from "@inertiajs/vue3";
+import { Breadcrumb, Button } from "primevue";
+import AppHeader from "../Components/AppHeader.vue";
 
 let pageVariables = defineProps({
     debts: Object,
@@ -93,8 +94,20 @@ let pageVariables = defineProps({
 });
 
 provide("translate", "debts");
+const items = ref([{ label: "Home", route: "/" }, { label: "Debt" }]);
 
 function getPercent(fullSum = 0, sum = 0) {
     return Number(((sum / fullSum) * 100).toFixed());
+}
+
+function deleteDebt(id) {
+    router.delete(route("debt.destroy", id), {
+        onSuccess: () => {
+            router.visit(route("debt.index"), { only: [] });
+        },
+        onError: (errors) => {
+            console.error("Delete failed:", errors);
+        },
+    });
 }
 </script>
